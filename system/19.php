d2e9755908dae6b47ec57cca909c6a3f73f4ab2c
mohamed <mohamed.alaa.el.din.hassan@gmail.com>
2018-07-24 11:33:16 +0200

    first commit

---
+++ system/19.php
@@ -0,0 +1,67 @@
+<php include ('header.php');?>
+<section id="main-content">
+	<section class="wrapper">
+        <div class="form-group">
+            <div class="row">
+                <section class="panel">
+                    <header class="panel-heading">
+                        Inline form
+                    </header>
+                </section>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="col-md-3">
+                        <label class="control-label">Iconic</label>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="input-group m-bot15">
+                            <span class="input-group-addon btn-white"><i class="fa fa-user"></i></span>
+                            <input type="text" class="form-control" placeholder="Username">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="col-md-3">
+                        <label class="control-label">Iconic</label>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="input-group m-bot15">
+                            <span class="input-group-addon btn-white"><i class="fa fa-user"></i></span>
+                            <input type="text" class="form-control" placeholder="Username">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <a href="#"  class="btn btn-danger">
+                        Form in Modal 3
+                    </a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="col-md-3">
+                        <label class="control-label">Iconic</label>
+                    </div>
+                    <div class="col-md-9">
+                        <select class="form-control m-bot15">
+                            <option>Option 1</option>
+                            <option>Option 2</option>
+                            <option>Option 3</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+ <!-- footer -->
+		  <div class="footer">
+			<div class="wthree-copyright">
+			  <p>© 2017 Visitors. All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+			</div>
+		  </div>
+  <!-- / footer -->
+</section>
+<!--main content end-->
+<php include ('footer.php');?>
